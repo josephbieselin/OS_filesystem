41b1b97f3eede7c5a001f6651fd1a5e44dd6f51a
Joseph Bieselin <joebieselin@gmail.com>
2015-03-27 02:59:48 -0400

    Beginning of C code for filesystem

---
+++ jabFS.c
@@ -0,0 +1,30 @@
+/*
+	Joseph Bieselin
+	N 16590510
+	jab975
+
+	HW 2:
+	Filesystem
+*/
+
+#include <stdlib.h>	// utility functions: atoi, atol
+#include <stdio.h>	// printf, fprintf, fgets, fputs
+#include <string.h>	// strcpy, strcmp, strncmp, strtok, strlen
+#include <assert.h>	// debugging: assert
+
+
+/*
+DEFINED CONSTANTS TO BE USED
+#define ____ ###
+*/
+
+int main(int argc, char *argv[])
+{
+	int fuse_stat;
+	// Check that at least 2 arguments were passed: root and mount point
+	if(argc < 3)
+	{
+		// not enough arguments so print to standard error and abort
+		fprintf(stderr
+	}
+}
